54633addd817855322c2a49d9613d609a4e98728
hayanLee <hayanlee.dev@gmail.com>
2024-10-08 16:33:06 +0900

    Add : Init PWA and project
    - icon, manifest, gitignore

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,5 @@
+import withPWA from 'next-pwa';
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
 
-export default nextConfig;
+export default withPWA({ ...nextConfig, dest: 'public' });

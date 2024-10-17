9b1a9aa431b91d946a1be5ddb53832a4db230e10
hayanLee <hayanlee.dev@gmail.com>
2024-10-17 21:27:59 +0900

    Fix : PWA workbox 비활성화

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,10 @@
 import withPWA from 'next-pwa';
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
 
-export default withPWA({ ...nextConfig, dest: 'public' });
+export default withPWA({
+    ...nextConfig,
+    dest: 'public',
+    disable: process.env.NODE_ENV === 'development',
+});

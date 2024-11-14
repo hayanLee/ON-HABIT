0109a226ef09f70bf7f836da21ada7581e52f1e3
hayanLee <hayanlee.dev@gmail.com>
2024-11-15 02:06:38 +0900

    BugFix : Progress width 수정
    - tailwindCSS 동적 스타일링 적용 안됨

--- src/components/Progress.tsx
+++ src/components/Progress.tsx
@@ -5,9 +5,13 @@ import clsx from 'clsx';
 const Progress = () => {
     const progress = useProgressStore((state) => state.progress);
     const step = useProgressStore((state) => state.step);
+
     return (
         <div className='w-full bg-gray-50 rounded-full h-3'>
-            <div className={clsx(`bg-main h-3 rounded-full w-${progress}/${step} transition-all duration-500`)} />
+            <div
+                className={clsx(`bg-main h-3 rounded-full transition-all duration-500`)}
+                style={{ width: `${(progress / step) * 100}%` }}
+            />
         </div>
     );
 };
